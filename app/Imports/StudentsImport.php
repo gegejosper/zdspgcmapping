0ab8ddced5218b176f220f70c07a4ea489cf5de8
Gegejosper Ceniza <gegejosper@gmail.com>
2024-12-17 08:26:29 +0800

    row skip

--- app/Imports/StudentsImport.php
+++ app/Imports/StudentsImport.php
@@ -4,56 +4,25 @@ namespace App\Imports;
 
 use App\Models\Student;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithStartRow;
 
-class StudentsImport implements ToModel
+class StudentsImport implements ToModel, WithStartRow
 {
     /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
-
+     * Start processing from the 2nd row of the file (skip header row).
+     */
     public function startRow(): int
     {
-        return 2; // Start processing from the 2nd row
+        return 2;
     }
+
+    /**
+     * Define how each row is mapped to the Student model.
+     * @param array $row
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
     public function model(array $row)
     {
-        // $existing_student = Student::where([
-        //     ['first_name', $row[0]],
-        //     ['last_name', $row[1]],
-        //     ['middle_name', $row[2] ?? null],
-        //     ['municipality', $row[3]],
-        //     ['province', $row[4]],
-        //     ['region', $row[5] ?? null],
-        //     ['course_id', $row[6]],
-        //     ['campus_id', $row[7]],
-        //     ['scholarship_id', $row[8]],
-        // ])->first();
-        
-        // if ($existing_student) {
-        //     // Return a message or handle the duplicate record logic here
-        //     return null; // or you can log, skip, or throw an exception
-        // }
-        
-        // // If no existing student is found, create a new one
-        // return new Student([
-        //     'first_name'     => $row[0],
-        //     'last_name'      => $row[1],
-        //     'middle_name'    => $row[2] ?? null,
-        //     'address'        => null,
-        //     'municipality'   => $row[3],
-        //     'province'       => $row[4],
-        //     'region'         => $row[5] ?? null,
-        //     'latitude'       => null,
-        //     'longitude'      => null,
-        //     'course_id'      => $row[6],
-        //     'campus_id'      => $row[7],
-        //     'scholarship_id' => $row[8],
-        //     'year'           => '1st Year',
-        //     'status'         => 'active',
-        // ]);
-
         $existing_student = Student::where([
             ['first_name', $row[0]],
             ['last_name', $row[1]],
